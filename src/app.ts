089619069058e0a34aef90776b5ceffe3ff1123c
Irfan Q <irfan4902@gmail.com>
2023-08-24 15:56:54 +0800

    Added content to the webpages + more Deleted modify-file because it's not needed for this project Replaced folder.png with folder.svg

--- src/app.ts
+++ src/app.ts
@@ -5,22 +5,21 @@ import express from 'express';
 import index from './routes/index';
 import login from './routes/login';
 import file_system from './routes/file-system';
-import modify_file from './routes/modify-file';
 import jobs from './routes/jobs';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 export const viewPath = path.join(__dirname, '../public/views');
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
 app.use('/', index);
 app.use('/', login);
 app.use('/', file_system);
-app.use('/', modify_file);
 app.use('/', jobs);
 
 // Start the server
