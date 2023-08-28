ebdd99e87ee5bb32ea89aba74bb44ce0671cb7f0
Irfan Q <irfan4902@gmail.com>
2023-08-28 11:03:06 +0800

    Added pagination in API

--- src/routes/file-system.ts
+++ src/routes/file-system.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import url from 'url';
 // import archiver from 'archiver';
 import fs from "fs";
 import path from "path";
@@ -9,15 +8,28 @@ const router = express.Router();
 
 // Name of the root directory in the client (displayed on the breadcrumbs)
 const homeName = "File System";
-const rootPath = "D:/Irfan's Files/Southampton";
+const rootPath = "C:\\Users\\irfan.aslam\\Desktop\\test folder";
 
-// Typescript
+// Typescript types
 type FileData = {
     name: string;
     date: string;
     type: string;
     size: string;
     url: string;
+};
+
+type PaginatedResults = {
+    results: FileData[];
+    totalPages: number;
+    next?: {
+        page: number;
+        limit: number;
+    };
+    prev?: {
+        page: number;
+        limit: number;
+    };
 };
 
 //
@@ -30,8 +42,13 @@ router.get('/file-system', (req, res) => {
 
 router.get('/file-system/home', async (req, res) => {
     try {
-        const hi = await sendFiles(rootPath);
-        res.status(200).json(hi);
+        let page = parseInt(req.query.page as string) || 1;
+        let limit = parseInt(req.query.limit as string) || 5;
+        let search = (req.query.search as string) || "";
+        let sort = (req.query.sort as string) || "";
+
+        const result = await processData(page, limit, search, sort);
+        res.status(200).json(result);
     } catch (err) {
         res.status(500).send('Internal Server Error');
     }
@@ -47,17 +64,17 @@ router.get('/file-system/home', async (req, res) => {
  * Sends file data for the given directory.
  * @param {string} directory - Directory path
  */
-async function sendFiles(directory: string): Promise<FileData[]> {
+async function getData(directory: string): Promise<FileData[]> {
     const fileData: FileData[] = [];
 
     console.log("Current Directory:", directory);
 
     try {
         const items: string[] = await fs.promises.readdir(directory);
-    
+
         // For every item in items, get the file data and add it to statPromises
         const statPromises: Promise<FileData>[] = items.map(async itemName => {
-            const filePath= path.join(directory, itemName);
+            const filePath = path.join(directory, itemName);
             const stats = await fs.promises.stat(filePath);
             return {
                 name: itemName,
@@ -67,7 +84,7 @@ async function sendFiles(directory: string): Promise<FileData[]> {
                 url: filePath.replaceAll('\\', '/').replace(rootPath, homeName)
             }
         });
-    
+
         // Push everything from statPromises to fileData
         fileData.push(...await Promise.all(statPromises));
 
@@ -75,7 +92,7 @@ async function sendFiles(directory: string): Promise<FileData[]> {
         console.log("Error reading folder:", err);
         throw err;
     }
-    
+
     console.log(fileData);
     return fileData;
 }
@@ -90,6 +107,37 @@ function formatFileSize(bytes: number): string {
     if (bytes < 1024 * 1024) return (bytes / 1024).toFixed(2) + ' KB';
     if (bytes < 1024 * 1024 * 1024) return (bytes / (1024 * 1024)).toFixed(2) + ' MB';
     return (bytes / (1024 * 1024 * 1024)).toFixed(2) + ' GB';
+}
+
+function paginateData(model: FileData[], page: number, limit: number): PaginatedResults {
+    const startIndex = (page - 1) * limit;
+    const endIndex = startIndex + limit;
+
+    const results: PaginatedResults = {
+        results: model.slice(startIndex, endIndex),
+        totalPages: Math.ceil(model.length / limit)
+    };
+
+    if (endIndex < model.length) {
+        results.next = {
+            page: page + 1,
+            limit: limit,
+        };
+    }
+
+    if (startIndex > 0) {
+        results.prev = {
+            page: page - 1,
+            limit: limit,
+        };
+    }
+
+    return results;
+}
+
+async function processData(page: number, limit: number, search: string, sort: string) {
+    console.log(`page: ${page}, limit: ${limit}, search: ${search}, sort: ${sort}`);
+    return paginateData(await getData(rootPath), page, limit);
 }
 
 export default router;
